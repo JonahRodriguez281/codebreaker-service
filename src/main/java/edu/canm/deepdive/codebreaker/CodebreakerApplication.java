adfa1d43c2a267f583036f7e0836be15b8110eef
Jonah Rodriguez <jonahrodriguez95@gmail.com>
2020-10-23 10:03:32 -0600

    Initial commit.

---
+++ src/main/java/edu/canm/deepdive/codebreaker/CodebreakerApplication.java
@@ -0,0 +1,13 @@
+package edu.canm.deepdive.codebreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebreakerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodebreakerApplication.class, args);
+  }
+
+}
